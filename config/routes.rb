cd0c3e98c48cbd79954562fc08188b9aa86197fe
yamashita2902 <yptkyyp57575@gmail.com>
2025-06-09 15:47:53 +0900

    homeコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-   root to: "meals#index"
+   root to: "homes#index"
+   resources :homes
    resources :meals
    resources :questions
 end
